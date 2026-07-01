256ac83cfbc51054529de77bcec43349cfbd7088
goutamr6360@gmail.com <gR#321654>
2026-07-01 22:11:15 +0530

    feat(domain): add base domain entities

---
+++ src/SmartHRMS.Domain/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using SmartHRMS.Domain.Common;
+using SmartHRMS.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHRMS.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string EmployeeCode { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public DateOnly JoiningDate { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public EmploymentStatus EmploymentStatus { get; set; }
+
+        public Guid CompanyId { get; set; }
+
+        public Guid DepartmentId { get; set; }
+
+        public Guid DesignationId { get; set; }
+    }
+}
---
+++ src/SmartHRMS.Domain/Enums/EmploymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHRMS.Domain.Enums
+{
+    public enum EmploymentStatus
+    {
+        Active = 1,
+        Inactive = 2,
+        OnLeave = 3,
+        Terminated = 4
+    }
+}
