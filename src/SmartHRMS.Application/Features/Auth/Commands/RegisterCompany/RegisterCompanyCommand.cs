357b8d7ad8a491d0a848149296a971656e09c449
goutamr6360@gmail.com <gR#321654>
2026-07-08 13:55:30 +0530

    feat(auth): add auth backend foundation and Angular auth UI setup

---
+++ src/SmartHRMS.Application/Features/Auth/Commands/RegisterCompany/RegisterCompanyCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHRMS.Application.Features.Auth.Commands.RegisterCompany
+{
+    public class RegisterCompanyCommand : IRequest<Guid>
+    {
+
+    }
+}
---
+++ src/SmartHRMS.Application/Features/Auth/Commands/RegisterCompany/RegisterCompanyCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHRMS.Application.Features.Auth.Commands.RegisterCompany
+{
+    internal class RegisterCompanyCommandHandler
+    {
+    }
+}
